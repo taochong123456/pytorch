f9288b0c053d82020bf237c26cba3780fbc32b5c
KarhouTam <karhou.tam@outlook.com>
2026-07-08 02:40:02 +0000

    [3/N][Test] Migrate TestBwdGradients device-agnostic skips to `skipOps`
    ghstack-source-id: a6d481c83daff32b5a165fbfc3f854e83e7ce02c
    Pull-Request: https://github.com/pytorch/pytorch/pull/184355

--- torch/testing/_internal/opinfo/definitions/linalg.py
+++ torch/testing/_internal/opinfo/definitions/linalg.py
@@ -1642,7 +1642,6 @@ op_db: list[OpInfo] = [
             # we skip gradient checks for this suite as they are tested in
             # variant_test_name='grad_oriented'
             DecorateInfo(unittest.skip("Skipped!"), "TestFwdGradients"),
-            DecorateInfo(unittest.skip("Skipped!"), "TestBwdGradients"),
             # The values for attribute 'shape' do not match
             DecorateInfo(unittest.skip("Skipped!"), "TestCommon", "test_out"),
             DecorateInfo(
@@ -1809,9 +1808,6 @@ op_db: list[OpInfo] = [
         supports_fwgrad_bwgrad=True,
         skips=(
             DecorateInfo(
-                unittest.expectedFailure, "TestBwdGradients", "test_fn_gradgrad"
-            ),
-            DecorateInfo(
                 unittest.skip("Skipped!"),
                 "TestFakeTensor",
                 "test_fake_crossref_backward_amp",
@@ -1846,17 +1842,12 @@ op_db: list[OpInfo] = [
         supports_fwgrad_bwgrad=True,
         skips=(
             # [NEW] Skips specifically for sample inputs at zero
-            # norm's vjp/jvp are not well-conditioned near zero
-            DecorateInfo(
-                unittest.expectedFailure, "TestBwdGradients", "test_fn_gradgrad"
-            ),
             DecorateInfo(
                 unittest.expectedFailure, "TestFwdGradients", "test_fn_fwgrad_bwgrad"
             ),
             DecorateInfo(
                 unittest.expectedFailure, "TestFwdGradients", "test_forward_mode_AD"
             ),
-            DecorateInfo(unittest.expectedFailure, "TestBwdGradients", "test_fn_grad"),
         ),
     ),
     OpInfo(
